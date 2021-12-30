e6a6d5a2edce7b2fcbbe8801e8f4f35420aa5272
zhouduforgit <1974788603@qq.com>
2021-12-30 17:33:37 +0800

    经典爬楼梯算法

--- src/classicalalgorithm/ClimbStairs.java
+++ src/classicalalgorithm/ClimbStairs.java
@@ -2,12 +2,14 @@ package classicalalgorithm;
 
 /**
  * @Description TODO
+ * 经典算法题把楼梯 规定一次只能上1节楼梯或只能上2节楼梯。
+ * 问上到第n节楼梯一共有多少种方法
  * @Author 周都
  * @Date 2021/8/31 15:52
  */
 public class ClimbStairs {
     public static void main(String[] args) {
-        System.out.println(climbStairs(13));
+        System.out.println(climbStairs(26));
     }
     public static int climbStairs(int n) {
         if(n==1) {
